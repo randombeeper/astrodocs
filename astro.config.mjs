1338d7443ac4584294506584c604e6f3d7437fc8
James Gray <randombeeper@gmail.com>
2024-08-09 16:30:30 -0600

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+// your mom
 export default defineConfig({
     site: 'https://randombeeper.github.io',
     base: 'astrodocs',
