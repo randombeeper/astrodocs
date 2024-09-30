7fca5e019bea354c9155083da372d5073b8dd0ee
James Gray <randombeeper@gmail.com>
2024-09-30 15:36:26 -0600

    updates

---
+++ src/components/DocSearchAstro.jsx
@@ -0,0 +1,14 @@
+import DocSearch from '@docsearch/react';
+import '@docsearch/css';
+
+const DocSearchAstro = () => {
+    return (
+        <DocSearch
+          appId="3ZAQCSL5V0"
+          indexName="cortex"
+          apiKey="69aa153f53e4a4f9fa490742464716e2"
+        />
+      );
+};
+
+export default DocSearchAstro;
